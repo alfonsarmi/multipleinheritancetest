fe4ba302bb8fe308c48f38be9f678dfade55c2f0
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-02-12 11:55:48 +0100

    f

---
+++ new.js
@@ -0,0 +1,37 @@
+class Car0 {
+  constructor(brand) {    
+    this.carname = brand;
+  }
+  present0() {
+    return 'I have 1 ' + this.carname;
+  }
+}
+
+class Car1 extends Car0{
+  constructor(brand) {    
+    this.carname = brand;
+  }
+  present() {
+    return 'I have 1 ' + this.carname;
+  }
+}
+
+class Car2 {
+  constructor(brand) {    
+    this.carname = brand;
+  }
+  present() {
+    return 'I have 2 ' + this.carname;
+  }
+}
+
+
+class BaseTest extends Many(Car1, Car2) {  
+  constructor(brand, mod) {
+    super(brand);
+    this.model = mod;
+  }
+  show() {
+    return this.present() + ', it is a ' + this.model;
+  }
+}
